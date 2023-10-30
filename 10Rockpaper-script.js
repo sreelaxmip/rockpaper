9591921b015b90361e87d7855384f0d9472f46fc
sreelaxmip <133085650+sreelaxmip@users.noreply.github.com>
2023-10-30 22:29:28 +0530

    Add files via upload

---
+++ 10Rockpaper-script.js
@@ -0,0 +1,86 @@
+let score = JSON.parse(localStorage.getItem('score')); 
+  updateScore();
+      function playGame(playerMove){
+        const computerMove=pickComputerMove();
+   
+   let result= '';
+
+  if(playerMove === 'scissors'){
+    if(computerMove === 'rock'){
+     result='you lose.';
+   }else if(computerMove ==='paper'){
+     result ='you win.';
+   }else if(computerMove==='scissors')
+   {
+     result=' tie.';
+   }
+
+  }else if(playerMove=== 'paper'){
+
+    if (computerMove === 'rock'){
+    result='you win.';
+  }else if(computerMove==='paper'){
+    result ='tie.';
+  }else if(computerMove==='scissors')
+  {
+    result='you lose.';
+  }
+}else if( playerMove ==='rock'){
+
+  if (computerMove === 'rock'){
+    result='tie.';
+  }else if(computerMove==='paper'){
+    result ='you lose.'
+  }else if(computerMove==='scissors')
+  {
+    result='you win.';
+  }
+
+}
+
+if(result === 'you win.'){
+  score.wins += 1;
+
+}else if(result === 'you lose.'){
+  score.losses+=1;
+
+}else if(result === 'tie.'){
+  score. ties +=1;
+}
+
+localStorage.setItem('score', JSON.stringify(score));
+
+updateScore();
+
+document.querySelector('.js-result').innerHTML= result;
+
+document.querySelector('.js-move').innerHTML=`YOU
+   <img src="${playerMove}-emoji1.png" 
+   class="image-icon">
+<img src="${computerMove}-emoji1.png"
+ class="image-icon">Computer`;
+       }
+   function updateScore(){
+    document.querySelector('.js-score')
+    .innerHTML = `wins:${score.wins},losses:${score.losses},ties:${score.ties}`;
+
+   }
+        function pickComputerMove()
+        {
+          const randomNumber=Math.random();
+  
+          let computerMove='';
+  
+    if(randomNumber >= 0 && randomNumber<1/3){
+       computerMove='rock'; 
+    }
+    else if(randomNumber >=1/3 && randomNumber < 2/3){
+       computerMove='paper';
+    }
+    else if(randomNumber >= 2/3 && randomNumber <1)
+    {
+       computerMove='scissors';
+    }
+
+    return computerMove;  
+        }
